7fa812e8a7448e513ba58f6fd8d1aa6153d0c64f
Banjos <48971965+brunosilveiradosanjos@users.noreply.github.com>
2024-12-15 15:14:12 -0300

    feat(setup): theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from '@screens/Groups/Groups'
 import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components'
+import theme from './src/theme'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar />
       <Groups />
-    </>
+    </ThemeProvider>
   )
 }
